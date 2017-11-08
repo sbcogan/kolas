f8d40ec06852605b756799c67a8ddd5489811639
Clara Forrestal <clara.forrestal@gmail.com>
2017-11-08 13:05:58 -0500

    Teams scene - fake data

--- src/scenes/Teams/Teams.js
+++ src/scenes/Teams/Teams.js
@@ -1,10 +1,68 @@
 // @flow
 import React from 'react';
 import Layout from 'components/Layout';
+import { Flex } from 'reflexbox';
+import { Table, Button, Input, Select } from 'antd';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name'
+  },
+  {
+    title: 'Gender',
+    dataIndex: 'gender',
+    key: 'gender'
+  },
+  {
+    title: 'Region',
+    dataIndex: 'region',
+    key: 'region'
+  }
+];
+
+// Fake the data for now
+const teams = [
+  {
+    id: 1,
+    name: 'Duke',
+    gender: 'mens'
+  },
+  {
+    id: 2,
+    name: 'Duke',
+    gender: 'womens'
+  },
+  {
+    id: 3,
+    name: 'UNC',
+    gender: 'mens'
+  },
+  {
+    id: 4,
+    name: 'UNC',
+    gender: 'womens'
+  }
+];
 
 class Teams extends React.Component<{}> {
   render() {
-    return <Layout>Teams</Layout>;
+    return (
+      <Layout>
+        <Flex column auto>
+          <Table
+            bordered
+            title={() => 'Teams'}
+            dataSource={teams}
+            columns={columns}
+            pagination={{
+              defaultPageSize: 5
+            }}
+          />
+        </Flex>
+      </Layout>
+    );
   }
 }
 
