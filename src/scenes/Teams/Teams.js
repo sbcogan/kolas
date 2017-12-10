360581c9003d95810c6bab9f6d7c4c91e07292ae
Zachary Marion <zfm2@duke.edu>
2017-12-10 13:09:50 -0500

    Checkpoint

--- src/scenes/Teams/Teams.js
+++ src/scenes/Teams/Teams.js
@@ -2,25 +2,33 @@
 import React from 'react';
 import Layout from 'components/Layout';
 import { Flex } from 'reflexbox';
+import { Link } from 'react-router-dom';
 import { observer } from 'mobx-react';
-import { Table, Spin } from 'antd';
+import { Spin } from 'antd';
 import styled from 'styled-components';
 import TeamsStore from './TeamsStore';
 
 type Props = {};
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name'
-  },
-  {
-    title: 'Gender',
-    dataIndex: 'gender',
-    key: 'gender'
+type TeamListItemProps = {
+  team: {
+    ID: string,
+    name: string,
+    gender: string
   }
-];
+};
+
+const TeamListItem = ({ team: { ID, name, gender } }: TeamListItemProps) =>
+  <Link to={`/teams/${ID}`}>
+    <TeamItem>
+      <h3>
+        {name}
+      </h3>
+      <span>
+        {gender}
+      </span>
+    </TeamItem>
+  </Link>;
 
 @observer
 class Teams extends React.Component<Props> {
@@ -41,26 +49,27 @@ class Teams extends React.Component<Props> {
         <Flex column auto>
           {this.store.loading
             ? <Spin />
-            : <FlexTable
-                bordered
-                title={() => 'Teams'}
-                dataSource={this.store.teams}
-                columns={columns}
-                pagination={{
-                  defaultPageSize: 5
-                }}
-              />}
+            : <Flex column m={3}>
+                {this.store.teams.map((team, i) =>
+                  <TeamListItem team={team} key={i} />
+                )}
+              </Flex>}
         </Flex>
       </Layout>
     );
   }
 }
 
-const FlexTable = styled(Table)`
-  margin: 30px;
-  .ant-table {
-    background: #fff;
-  }
+const TeamItem = styled(Flex)`
+  background: #fff;
+  border-radius: 2px;
+  padding: 10px;
+  margin-bottom: 10px;
+  cursor: pointer
+  &:hover {
+    box-shadow: 0 1px 6px rgba(0, 0, 0, 0.2);
+    border-color: transparent;
+  } 
 `;
 
 export default Teams;
